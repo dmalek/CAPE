0758209e1fc149e9c3a365ad14bc5ee55b3137ad
Dalibor Malek <daliborm@in2.hr>
2026-04-13 13:08:05 +0200

    implementacija addtodoendpoint logike

--- CAPE/Endpoints/Todo/Add/AddTodoEndpoint.cs
+++ CAPE/Endpoints/Todo/Add/AddTodoEndpoint.cs
@@ -1,12 +1,25 @@
-﻿namespace CAPE.Endpoints.Todo.Add;
+﻿using CAPE.Context;
+using CAPE.Context.Models;
+
+namespace CAPE.Endpoints.Todo.Add;
 
 public static class AddTodoEndpoint
 {
     public static void MapAddTodo(this IEndpointRouteBuilder app)
     {
-        app.MapPost("/todo", async (AddTodoRequest request) =>
+        app.MapPost("/todo", async (AddTodoRequest request, AppDbContext context) =>
         {
-            // logika
+            context.Add(new TodoItem
+            {
+                Title = request.Title,
+                Description = request.Description,
+                UserEmail = request.UserEmail,
+                DueDate = request.DueDate.Value,
+                Status = request.Status
+            });
+
+            await context.SaveChangesAsync();
+
             return Results.Ok(new AddTodoResponse());
         });
     }
